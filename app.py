96481e72786fce5ed3f1ba23d2ef5a11f2f4156e
techsmj <joyetuks@gmail.com>
2019-06-02 16:59:44 -0500

    add line

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
-
+import sqlalchemy.dialects.sqlite
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
